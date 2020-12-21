bb511873be0a99b1314ca1af60ff95fe80f1f27c
XProjects B.Hummel <infos@x-projects.de>
2020-12-21 19:21:21 +0100

    Adding icon and clas

--- contao-projects/src/Events/ProjectsAlpdeskFrontendViewListener.php
+++ contao-projects/src/Events/ProjectsAlpdeskFrontendViewListener.php
@@ -10,10 +10,15 @@ use Contao\Database;
 
 class ProjectsAlpdeskFrontendViewListener {
 
+  private static $icon = '../../../system/themes/flexible/icons/tablewizard.svg';
+  private static $iconclass = 'tl_projects_baritem';
+
   public function __invoke(AlpdeskFrontendeditingEventElement $event): void {
 
     if ($event->getElement()->type === 'xprojects_overview') {
       $event->getItem()->setValid(true);
+      $event->getItem()->setIcon(self::$icon);
+      $event->getItem()->setIconclass(self::$iconclass);
       $event->getItem()->setPath('do=xprojects');
       $event->getItem()->setLabel($GLOBALS['TL_LANG']['projects_label']);
     } else if ($event->getElement()->type === 'xprojects_detail') {
@@ -23,6 +28,8 @@ class ProjectsAlpdeskFrontendViewListener {
         $projectObj = Database::getInstance()->prepare("SELECT id FROM tl_xprojects WHERE alias=?")->execute($alias);
         if ($projectObj->numRows > 0) {
           $event->getItem()->setValid(true);
+          $event->getItem()->setIcon(self::$icon);
+          $event->getItem()->setIconclass(self::$iconclass);
           $event->getItem()->setPath('do=xprojects&table=tl_content&id=' . $projectObj->id);
           $event->getItem()->setLabel($GLOBALS['TL_LANG']['projects_label']);
         }
